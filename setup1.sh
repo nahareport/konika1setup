8b577dc500587c99439d8d1d794081e45e14d47f
nahareport <37258940+nahareport@users.noreply.github.com>
2026-06-26 14:43:07 +0900

    Update setup1.sh
    add DisableUpdate

--- setup1.sh
+++ setup1.sh
@@ -1,2 +1,3 @@
+echo -en "1" > /data/params/d/DisableUpdates
 printf '#!/usr/bin/env bash\n\nexport API_HOST=https://api.konik.ai/\nexport ATHENA_HOST=wss://athena.konik.ai\n\nexec ./launch_chffrplus.sh\n' > launch_openpilot.sh
 rm /data/params/d/DongleId && sudo reboot
